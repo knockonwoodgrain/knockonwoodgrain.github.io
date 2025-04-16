8e01bb10a0198a42a7ce09f4d9ef64196d06561e
parthjoshioriginal <parthnjoshi33@gmail.com>
2025-04-17 04:38:37 +0530

    added Packaging Design and Updated Simple Design Project

--- src/About.tsx
+++ src/About.tsx
@@ -9,7 +9,7 @@ function About() {
   Hi, I'm Parth Joshi, and I have a deep-rooted passion for learning, education, and exploration. I love navigating complex ideas and understanding phisolophical and political pradigms. I value equity and human connection. I thus immerse myself through diverse mediums and perspectives. This is a reflection of my journey, where knowledge, curiosity, passion, and love intertwine. Through visual and auditory artistry and design, <br /> I bring perspectives to life.
           <AboutIcons />
         </div>
-        <img className="me" src="Thumbnail/Me.jpg"></img>
+        <img className="me" src="Thumbnail/Me.webp"></img>
       </div>
     </>
   );
---
+++ src/Pages/Amrut.tsx
@@ -0,0 +1,31 @@
+import Project from "./Project";
+
+const AmrutPage = {
+  title: "Amrut",
+  text: "Amrut is a Luxury Indian Perfumery. A Packaging & Branding Design project in collaboration with Sau Yanaginouchi",
+  download: "Amrut/Amrut.pdf",
+  layout: "PresentationScrolling",
+  Content: [
+    "Amrut/Amrut Deck2.webp",
+    "Amrut/Amrut Deck3.gif",
+    "Amrut/Amrut Deck4.webp",
+    "Amrut/Amrut Deck5.gif",
+    "Amrut/Amrut Deck6.webp",
+    "Amrut/Amrut Deck7.webp",
+    "Amrut/Amrut Deck8.webp",
+    "Amrut/Amrut Deck9.gif",
+    "Amrut/Amrut Deck10.webp",
+    "Amrut/Amrut Deck11.webp",
+    "Amrut/Amrut Deck12.webp",
+    "Amrut/Master Doc.webp",
+    "Amrut/Master Doc2.webp",
+    "Amrut/Master Doc3.webp",
+    "Amrut/Master Doc4.webp",
+    "Amrut/Master Doc5.webp",
+  ],
+};
+function Amrut() {
+  return <Project {...AmrutPage} />;
+}
+
+export default Amrut;
